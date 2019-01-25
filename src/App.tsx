ea2361870926141726e94790b2b2c07f5900d3b9
Andrew Gierens <agierens@jdlf.com.au>
2019-01-25 14:28:35 +1100

    Removed android and ios

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import { Home } from './pages/home';
-
-const AppNavigator = createStackNavigator({
-  Home,
-});
-
-export default createAppContainer(AppNavigator);
