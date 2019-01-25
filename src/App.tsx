4f8821f7ae6aa37e8a66b47258b34b6c8ab718b1
Andrew Gierens <agierens@jdlf.com.au>
2019-01-25 14:32:22 +1100

    Revert "Removed android and ios"
    This reverts commit ea2361870926141726e94790b2b2c07f5900d3b9.

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { Home } from './pages/home';
+
+const AppNavigator = createStackNavigator({
+  Home,
+});
+
+export default createAppContainer(AppNavigator);
